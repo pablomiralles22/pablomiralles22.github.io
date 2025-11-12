c5381e51dca06ca1db210580ea635e47af85e275
Pablo Miralles <pablomiralles99@gmail.com>
2025-11-12 12:27:11 +0100

    Make sure CNAME is not reset

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 git worktree add --force _deploy gh-pages
 find _deploy -mindepth 1 -not -name '.git' -exec rm -rf {} +
 cp -r _site/* _deploy/
+rm _deploy/deploy.sh
+
+# Create CNAME file for custom domain
+echo "pablomirallesg.com" > _deploy/CNAME
+
 
 cd _deploy
 git add --all
